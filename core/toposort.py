e2812eb1a07cc69ad7c5d730475e101513482fbf
User <user@example.com>
2026-04-08 22:49:42 +0900

    stable: topo sort fixed + working DAG execution order

--- core/toposort.py
+++ core/toposort.py
@@ -2,15 +2,22 @@ from collections import defaultdict, deque
 
 def topological_sort(graph):
     indegree = defaultdict(int)
+    adj = defaultdict(list)
 
-    for node in graph:
-        indegree[node] = 0
+    nodes = set(graph.keys())
 
+    # build adjacency + nodes
     for node, deps in graph.items():
         for d in deps:
+            adj[d].append(node)
             indegree[node] += 1
+            nodes.add(d)
 
-    q = deque([n for n in graph if indegree[n] == 0])
+    # init missing nodes
+    for n in nodes:
+        indegree[n] = indegree[n]
+
+    q = deque([n for n in nodes if indegree[n] == 0])
 
     order = []
 
@@ -18,13 +25,12 @@ def topological_sort(graph):
         node = q.popleft()
         order.append(node)
 
-        for nxt in graph:
-            if node in graph[nxt]:
-                indegree[nxt] -= 1
-                if indegree[nxt] == 0:
-                    q.append(nxt)
+        for nxt in adj[node]:
+            indegree[nxt] -= 1
+            if indegree[nxt] == 0:
+                q.append(nxt)
 
-    if len(order) != len(graph):
+    if len(order) != len(nodes):
         raise Exception("Cycle detected")
 
     return order
